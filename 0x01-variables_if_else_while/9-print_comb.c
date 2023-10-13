c352d8e1d3ac0179e5b2affd4eb8b81aa19caa04
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-10-13 07:20:56 -0700

    program that prints all single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,19 @@ int main(void)
 {
 	int i;
 	/* printing all single digits separated by a comma */
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 	putchar('0' + i);
+	if (i == 9)
+	{
+	continue;
+	}
+	else
+	{
 	putchar(',');
 	putchar(' ');
 	}
-	putchar('0' + 9);
+	}
+	putchar('\n');
 	return (0);
 }
