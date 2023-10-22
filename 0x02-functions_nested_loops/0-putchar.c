670880877d6dd798996a3beb000f8ce26a6f7623
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-10-22 09:59:13 -0700

    first file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,7 +8,7 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar("p");
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -17,4 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+	return (0);
 }
