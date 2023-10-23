5966527658248a8742c6d2753f9a39648e5e4698
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-10-22 22:33:12 -0700

    function that prints all the alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints out all the alphabets in lower case
+  * main - uses built in functions
+  * Return: No return type
+  */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
