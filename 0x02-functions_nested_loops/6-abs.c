0264aab071329b489810fbc3a5d95508de36fc81
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-10-23 05:16:34 -0700

    program that computes the absolute value of an int type input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _abs - gets the absolute value of an input
+  * @i: input whose absolute value is to be determined
+  * Return: i or k
+  */
+int _abs(int i)
+{
+	const int j = 0;
+	int k;
+
+	if (i < 0)
+	{
+	k = j - i;
+	return (k);
+	}
+	else
+	{
+	return (i);
+	}
+}
