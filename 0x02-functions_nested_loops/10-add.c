a3bf384a7fa4c8dcfa54dcd2d2c27d0bd147859d
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-10-31 04:47:35 -0700

    program that takes in two integers and returne their sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * add - adds two integers and return the result
+  * @a: first integer to be added to second integer
+  * @b: second integer to be added to first integer
+  * Return: returns the value of the sum
+  */
+int add(int a, int b)
+{
+	int i;
+
+	i = a + b;
+
+	return (i);
+}
