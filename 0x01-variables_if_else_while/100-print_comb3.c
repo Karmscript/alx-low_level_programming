2e5d7f5e366a83ca0f0833278e420d5326eb9b2d
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-07 07:03:35 -0800

    program that prints some of the numbers below 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-int nums;
-int nums1;
+	int i;
 
-for (nums = '0'; nums < '9'; nums++)
-{
-for (nums1 = '0'; nums1 <= '9'; nums1++)
-{
-if (nums == '0' && nums1 < '1')
-continue;
-if (nums == '1' && nums1 < '2')
-continue;
-if (nums == '2' && nums1 < '3')
-continue;
-if (nums == '3' && nums1 < '4')
-continue;
-if (nums == '4' && nums1 < '5')
-continue;
-if (nums == '5' && nums1 < '6')
-continue;
-if (nums == '6' && nums1 < '7')
-continue;
-if (nums == '7' && nums1 < '8')
-continue;
-if (nums == '8' && nums1 < '9')
-continue;
-putchar(nums);
-putchar(nums1);
-if (nums == '8' && nums1 == '9')
-break;
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i <= 99; i++)
+	{
+	if ((i / 10) < (i % 10))
+	{
+	if (i != 89)
+	{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar(8 + '0');
+	putchar(9 + '0');
+	return (0);
 }
