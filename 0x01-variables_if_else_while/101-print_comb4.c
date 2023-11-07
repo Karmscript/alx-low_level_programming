2d4558438b807bf394a8bccf59d3872c48767f40
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-07 13:33:16 -0800

    program that prints 3 digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - function prints some three digit numbers
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int nums;
-int nums1;
-int nums2;
+	int i;
 
-for (nums = '0'; nums <= '7'; nums++)
-{
-for (nums1 = nums + 1; nums1 <= '9'; nums1++)
-{
-for (nums2 = nums1 + 1; nums2 <= '9'; nums2++)
-{
-putchar(nums);
-putchar(nums1);
-putchar(nums2);
-if (nums == '7' && nums1 == '8' && nums2 == '9')
-break;
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	for (i = 0; i <= 999; i++)
+	{
+	if (((i / 100) < ((i / 10) % 10)) && (((i / 10) % 10) < (i % 10)))
+	{
+	putchar((i / 100) + '0');
+	putchar(((i / 10) % 10) + '0');
+	putchar((i % 10) + '0');
+	if (i != 789)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
