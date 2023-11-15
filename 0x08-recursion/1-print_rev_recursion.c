792149406623b4d00bee4a929e2944f717b7ae56
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-15 07:32:52 -0800

    program that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - prints a string in reverse
+  * @s:m string whose reverse is to be printed
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
