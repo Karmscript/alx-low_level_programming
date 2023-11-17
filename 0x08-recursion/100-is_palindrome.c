4aea2b4d273f8d2243590b913f9978b936ccee39
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-16 22:58:19 -0800

    program that checks if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: the string to count
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
+  * _strlen_check - gets the length of the string
+  * @s: string to be counted
+  * Return: length of the string
+  */
+int _strlen_check(char *s)
 {
 	if (*s)
-	{
-		s++;
-		return (1 + _strlen_recursion(s));
-	}
+		return (1 + _strlen_check(s + 1));
 	return (0);
 }
+
 /**
- * checker - helper function for is_palindrome
- * @str: the string
- * @len: length of string
- * @count: counter of recursion
- * Return: 1 if string is a palindrome, 0 if it is not.
- */
-int checker(char *str, int len, int count)
+  * helper_func - helps to determine if a string is a palindrome
+  * @string: string to be checked for palindromicity
+  * @length: length of the string
+  * @counter: counter of string
+  * Return: 1 if string is a palindrome and 0 if it is not
+  */
+int helper_func(char *string, int counter, int length)
 {
-	if (count >= len)
+	if (counter >= length)
+	{
 		return (1);
-	if (str[len] == str[count])
-		return (checker(str, len - 1, count + 1));
+	}
+	if (string[counter] == string[length])
+		return (helper_func(string, counter + 1, length - 1));
 	return (0);
 }
+
 /**
- * is_palindrome - checks if the string is a palindrome
- * @s: the string to check
- * Return: 1 if string is a palindrome, 0 if it is not.
- */
+  * is_palindrome - checks if a string is a palindrome
+  * @s: string to be checked
+  * Return: 1 if string is a palindrome and 0 if it is not
+  */
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
-	int count = 0;
+	int length =  _strlen_check(s);
+	int counter = 0;
 
-	return (checker(s, len - 1, count));
-} 
+	return (helper_func(s, counter, length - 1));
+}
