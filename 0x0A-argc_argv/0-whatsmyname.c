e5a9429cc762657ca2fa0e38dfa4872ed31f9dd6
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-17 12:49:28 -0800

    test1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 
 /**
   * main - Entry point of the code
+  * @argc: indicates the number of arguments in main
+  * @argv: argument vector
   * Return: Always 0 (Success)
   */
 int main(int argc, char *argv[])
