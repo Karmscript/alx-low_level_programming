9590ea71cde791d86896f8e4bc7e16184955df8e
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-17 17:33:00 -0800

    eoe

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point of function
+  * @argc: argument counter
+  * @argv: the arguments themselves
+  * Return: 0 indicates success and 1 indicates error
+  */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	int a, b;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			if (isdigit(*argv[a]))
+					{
+						b = atoi(argv[a]);
+						i += b;
+						printf("%d\n", i);
+					}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
