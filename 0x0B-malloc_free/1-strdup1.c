24e298b0929f9e10c5baa7d86d9bc9179707c6df
Ramadan Ahmed <ahmedramadanbamidele@gmail.com>
2023-11-22 03:11:30 -0800

    test

--- 0x0B-malloc_free/1-strdup1.c
+++ 0x0B-malloc_free/1-strdup1.c
@@ -8,23 +8,21 @@
   */
 char *_strdup(char *str)
 {
-	unsigned len;
+	size_t str_length;
 	char *copy;
 	unsigned int i;
 
-	if (!str)
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str_length = strlen(str);
+	copy = malloc((str_length + 1) * sizeof(char));
+
+	if (copy == NULL)
 		return (NULL);
 
-	for (len = 0; str[len]; len++)
-		;
-
-	len++;
-	copy = malloc(len * sizeof(char));
-
-	if (!copy)
-		return (NULL);
-
-	for (i = 0; i <= len; i++)
+	for (i = 0; i <= str_length; i++)
 		copy[i] = str[i];
 
 	return (copy);
